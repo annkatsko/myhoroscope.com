3d58783e1d4ace7771cffc5fef14543ffc6b6dba
Hanna Katsko <1187484@gmail.com>
2022-10-06 13:35:39 +0300

    added cache fiture

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
 from . import views
-
-
-
-
+from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
-    path('learn_sign/', views.SignFormView.as_view(), name='learn-sign'),
-    path('', views.SignListView.as_view(), name='main-page'),
-    path('sign/', views.SignViewToUser.as_view(), name='your-sign'),
-    path('<slug:slug>/', views.SignDetailView.as_view(), name='sign-detail'),
-    path('<slug:slug>/<day>/', views.HoroscopeFormTemplateView.as_view(), name='horoscope')
+    path('learn_sign/', cache_page(60)(views.SignFormView.as_view()), name='learn-sign'),
+    path('', cache_page(60)(views.SignListView.as_view()), name='main-page'),
+    path('sign/', cache_page(60)(views.SignViewToUser.as_view()), name='your-sign'),
+    path('<slug:slug>/', cache_page(60)(views.SignDetailView.as_view()), name='sign-detail'),
+    path('<slug:slug>/<day>/', cache_page(60)(views.HoroscopeFormTemplateView.as_view()), name='horoscope')
 ]
