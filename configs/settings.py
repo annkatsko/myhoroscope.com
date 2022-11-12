6169bee31a676bb075fc3537afab4ccde6ea5840
Hanna Katsko <1187484@gmail.com>
2022-11-12 15:28:00 +0300

    settings modified

--- configs/settings.py
+++ configs/settings.py
@@ -176,10 +176,10 @@ LOGOUT_REDIRECT_URL = '/welcome/login'
 
 # Email configurations
 
-# EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_USE_TLS=True
-# EMAIL_HOST='smtp.gmail.com'
-# EMAIL_PORT=587
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
 # EMAIL_HOST_USER=env('EMAIL_HOST_USER')
 # EMAIL_HOST_PASSWORD=env('EMAIL_HOST_PASSWORD')
 # DEFAULT_FROM_EMAIL=env('DEFAULT_FROM_EMAIL')
