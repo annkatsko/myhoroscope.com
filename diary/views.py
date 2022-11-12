5dae0f06a297a428454ef1107e11fe1be274810a
Hanna Katsko <1187484@gmail.com>
2022-11-12 09:42:45 +0300

    template modifyed

--- diary/views.py
+++ diary/views.py
@@ -35,7 +35,7 @@ class PostDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Note
     fields = ['title', 'content']
-    success_url = 'dairy/'
+
 
     def form_valid(self, form):
         form.instance.author = self.request.user
